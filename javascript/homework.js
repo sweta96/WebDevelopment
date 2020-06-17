c5ec78b93ba39dfc26fde5a2b2015ab9c589df27
sweta_agarwal <sweta.agarwal96.sa@gmail.com>
2020-06-17 10:52:39 +0530

    Create homework.js

---
+++ javascript/homework.js
@@ -0,0 +1,13 @@
+function billingFunction()
+{
+  if(document.getElementById("same").checked){
+    var name = document.getElementById("shippingName").value;
+    var zip = document.getElementById("shippingZip").value;
+    document.getElementById("billingName").value = name;
+    document.getElementById("billingZip").value = zip;
+  }
+  else{
+    document.getElementById("billingName").value = null;
+    document.getElementById("billingZip").value = null;
+  }  
+}
